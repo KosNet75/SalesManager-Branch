720c08c2cce88af9a51991a8083d2f487c44529d
KosNet75 <nemokonstantin@gmail.com>
2022-07-30 18:06:40 +0700

    Final

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main {
 
+  public static void main(String[] args) {
+    SalesManager arreySales = new SalesManager(new long[]{1000, 900, 1400, 600, 2100, 850});
+    System.out.println("Наибольший чек продаж: " + arreySales.max() + " руб.");
+
+
+    System.out.println("Средний чек всех продаж: "  + arreySales.averageSales() + " руб.");
+    System.out.println("Всего продаж: " + arreySales.i);
+
+  }
+
+
 }
--- SalesManager.java
+++ SalesManager.java
@@ -1,19 +1,44 @@
+
 public class SalesManager {
+  int i=0;
+  protected long[] sales;
 
-  protected int[] sales;
-
-  public SalesManager(int[] sales) {
+  public SalesManager(long[] sales) {
     this.sales = sales;
   }
 
-  public int max() {
-    int max = -1;
-    for (int sale : sales) {
+
+  public long max() {
+    long max = -1;
+    for (long sale : sales) {
+
       if (sale > max) {
         max = sale;
       }
     }
     return max;
   }
-}
 
+  public long min() {
+    long min = sales[0];
+    for (long sale : sales) {
+      if (sale < min) {
+        min = sale;
+      }
+    }
+    return min;
+  }
+
+  public long averageSales() {
+    int average = 0;
+    for (long sale : sales) {
+      average += sale;
+       i=i+1;
+    }
+    return (average/i);
+  }
+
+
+
+
+}
